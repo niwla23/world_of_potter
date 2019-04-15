9c6662b3aa57b730c7e778cfb3b6d5f15b6505c0
Binyamin Green <binyaminagreen@gmail.com>
2019-04-15 10:02:20 -0400

    add roadmap

--- hpclothes/clothes.lua
+++ hpclothes/clothes.lua
@@ -1,29 +1,35 @@
-minetest.register_tool("hpclothes:school_hogwarts", {
+minetest.register_craftitem("hpclothes:school_hogwarts", {
 	description = "Standard School Clothes",
 	inventory_image = "clothes_school_hogwarts_inv.png",
 	groups = {clothes=1},
 })
 
-minetest.register_tool("hpclothes:school_gryfindor", {
+minetest.register_craftitem("hpclothes:school_gryfindor", {
 	description = "Gryfindor School Clothes",
 	inventory_image = "clothes_school_gryfindor_inv.png",
 	groups = {clothes=1},
 })
 
-minetest.register_tool("hpclothes:school_ravenclaw", {
+minetest.register_craftitem("hpclothes:school_ravenclaw", {
 	description = "Ravenclaw School Clothes",
 	inventory_image = "clothes_school_ravenclaw_inv.png",
 	groups = {clothes=1},
 })
 
-minetest.register_tool("hpclothes:school_hufflepuff", {
+minetest.register_craftitem("hpclothes:school_hufflepuff", {
 	description = "Hufflepuff School Clothes",
 	inventory_image = "clothes_school_hufflepuff_inv.png",
 	groups = {clothes=1},
 })
 
-minetest.register_tool("hpclothes:school_slytherin", {
+minetest.register_craftitem("hpclothes:school_slytherin", {
 	description = "Slytherin School Clothes",
 	inventory_image = "clothes_school_slytherin_inv.png",
+	groups = {clothes=1},
+})
+
+minetest.register_craftitem("hpclothes:red_robe", {
+	description = "Red Wizard Robes",
+	inventory_image = "clothes_red_robe_inv.png",
 	groups = {clothes=1},
 })
